bdb400654fc96aaa35eed6a40af468ac5efb4dc7
teresa-xu <teresa.xu1127@gmail.com>
2021-09-24 14:56:33 +0800

    Modify "about" screen and user account icon

--- app/src/main/java/com/orange/prophet/ui/AboutActivity.kt
+++ app/src/main/java/com/orange/prophet/ui/AboutActivity.kt
@@ -6,21 +6,17 @@ import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.orange.prophet.R
 
 
 class AboutActivity: AppCompatActivity() {
-    private lateinit var mMoreAppsButton: Button
+
     private lateinit var  mAbout: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
         supportActionBar?.hide();
-
-        mMoreAppsButton = findViewById(R.id.MoreApps)
-        mMoreAppsButton.setOnClickListener(mButtonListener)
 
         mAbout = findViewById(R.id.about_webview)
         mAbout.loadUrl("file:///android_asset/web/about.html")
@@ -28,17 +24,6 @@ class AboutActivity: AppCompatActivity() {
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
                 view.loadUrl(url)
                 return true
-            }
-        }
-    }
-
-    private var mButtonListener = View.OnClickListener { v ->
-        when (v.id) {
-            R.id.MoreApps -> {
-                //show more apps
-                val uri: Uri = Uri.parse("https://primezone.orange.com")
-                val intent = Intent(Intent.ACTION_VIEW, uri)
-                startActivity(intent)
             }
         }
     }
